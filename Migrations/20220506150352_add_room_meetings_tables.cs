e018836aaf03d4ff274cd520bcaa3a18c4ffd3d8
kimoo tareek <kimootareekk@outlook.com>
2022-05-06 17:44:54 +0200

    add room meetings migration

---
+++ Migrations/20220506150352_add_room_meetings_tables.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineLearning.Migrations
+{
+    public partial class add_room_meetings_tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LookupRoomMeetingStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    NameArabic = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    NameEnglish = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LookupRoomMeetingStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomMeetings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    StatusId = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    OwnerId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomMeetings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoomMeetings_LookupRoomMeetingStatuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "LookupRoomMeetingStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RoomMeetings_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomMeetings_RoomId",
+                table: "RoomMeetings",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomMeetings_StatusId",
+                table: "RoomMeetings",
+                column: "StatusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomMeetings");
+
+            migrationBuilder.DropTable(
+                name: "LookupRoomMeetingStatuses");
+        }
+    }
+}
